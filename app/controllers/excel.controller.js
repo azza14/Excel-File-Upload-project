8ee209ca3e344ccb239ca434bfa3eff206fe67d0
azza <azzaashry14@gmail.com>
2021-06-24 10:48:31 +0200

    add fun download excel

--- app/controllers/excel.controller.js
+++ app/controllers/excel.controller.js
@@ -4,53 +4,6 @@ const excel= require('exceljs')
 
 const readXlsxFile = require('read-excel-file/node');
 
-// const upload = async (req, res) => {
-//   try {
-//     if (req.file == undefined) {
-//       return res.status(400).send("Please upload an excel file!");
-//     }
-
-//     let path =
-//       __basedir + "/resources/static/assets/uploads/" + req.file.filename;
-
-//     readXlsxFile(path).then((rows) => {
-//       // skip header
-//       rows.shift();
-
-//       let tutorials = [];
-
-//       rows.forEach((row) => {
-//         let tutorial = {
-//           id: row[0],
-//           title: row[1],
-//           description: row[2],
-//           published: row[3],
-//         };
-
-//         tutorials.push(tutorial);
-//       });
-
-//       Tutorial.bulkCreate(tutorials)
-//         .then(() => {
-//           res.status(200).send({
-//             message: "Uploaded the file successfully: " + req.file.originalname,
-//           });
-//         })
-//         .catch((error) => {
-//           res.status(500).send({
-//             message: "Fail to import data into database!",
-//             error: error.message,
-//           });
-//         });
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send({
-//       message: "Could not upload the file: " + req.file.originalname,
-//     });
-//   }
-// };
-
 const upload = async (req, res) => {
 
     try {
@@ -153,8 +106,9 @@ const download= (req,res)=>{
         "Content-Disposition",
         "attachment; filename=" + "tutorials.xlsx"
          );
-  return worksheet.xlsx.write(res).then(function(){
-      res.status(200).end();
+
+      return workbook.xlsx.write(res).then(function () {
+        res.status(200).end();
   });
 
 });
